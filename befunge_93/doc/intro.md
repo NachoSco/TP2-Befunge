24afaaf5457955325631d8c26e1b75b251e359ae
Scopel Nicolas Ignacio <146841821+NachoSco@users.noreply.github.com>
2024-11-11 17:22:36 -0300

    Add files via upload

---
+++ befunge_93/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to befunge_93
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ befunge_93/src/befunge_93/core.clj
@@ -0,0 +1,39 @@
+(ns befunge-93.core
+    (:require [clojure.string :as str]))
+
+(defn leer-archivo [ruta]
+      (try
+        (let [contenido (slurp ruta)
+              contenido-limpio (str/replace contenido #"\r" "")]
+             (println contenido-limpio)
+             contenido-limpio)
+        (catch Exception e
+          (println "Error al leer el archivo:" (.getMessage e)))))
+
+(defn crear-matriz-vacia []
+      (vec (repeat 25 (vec (repeat 80 \space))))) ; Crea una matriz 25x80 llena de espacios
+
+(defn llenar-matriz [matriz contenido]
+      (loop [matriz matriz
+             chars (seq contenido)
+             fila 0
+             columna 0]
+            (if (empty? chars)
+              matriz ; Aquí retornamos la última versión de la matriz actualizada
+              (let [char (first chars)]
+                   (if (= char \newline)
+                     ;; Salto de línea: avanza a la siguiente fila y reinicia columna
+                     (recur matriz (rest chars) (inc fila) 0)
+                     ;; Inserta el carácter en la posición actual
+                     (let [nueva-fila (assoc (matriz fila) columna char) ; Actualizamos la fila
+                           nueva-matriz (assoc matriz fila nueva-fila)]  ; Actualizamos la matriz con la fila modificada
+                          (recur nueva-matriz (rest chars) fila (inc columna))))))))
+
+(defn -main []
+      (println "Contenido cargado:")
+      (let [contenido (leer-archivo "C:\\Users\\pochi\\Desktop\\ejemplo.txt")
+            matriz (crear-matriz-vacia)
+            matriz-llena (llenar-matriz matriz contenido)]
+           (println "Matriz llena:")
+           (doseq [fila matriz-llena]
+                  (prn fila))))
